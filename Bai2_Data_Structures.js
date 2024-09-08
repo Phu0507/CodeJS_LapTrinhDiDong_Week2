8649a50f61315c94915eec522a114b9a3b3ccffc
H82M19\Student <Student>
2024-09-08 10:32:55 +0700

    week2 code js ex1 and ex2

---
+++ Bai2_Data_Structures.js
@@ -0,0 +1,48 @@
+// Let's continue with our football betting app! Keep using the 'game' variable from 
+// before
+
+const game = {
+    team1: 'Bayern Munich',
+    team2: 'Borrussia Dortmund',
+    players: [
+    [
+    'Neuer',
+    'Pavard',
+    'Martinez',
+    'Alaba',
+    'Davies',
+    'Kimmich',
+    'Goretzka',
+    'Coman',
+    'Muller',
+    'Gnarby',
+    'Lewandowski',
+    ],
+    [
+    'Burki',
+    'Schulz',
+    'Hummels',
+    'Akanji',
+    'Hakimi',
+    'Weigl',
+    'Witsel',
+    'Hazard',
+    'Brandt',
+    'Sancho',
+    'Gotze',
+    ],
+    ],
+    score: '4:0',
+    scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+    'Hummels'],
+    date: 'Nov 9th, 2037',
+    odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+    },
+    };
+
+//     1.  Loop over the game.scored array and print each player name to the console, 
+// along with the goal number (Example: "Goal 1: Lewandowski")
+
